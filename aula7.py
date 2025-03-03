9490ee9948b3e090b120a962f479abd61d5bbc67
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-03 04:05:29 -0300

    exercicios

---
+++ aula7.py
@@ -0,0 +1,9 @@
+#introdução a variáveis em python
+
+nome = 'Johnny'
+idade = 30
+maior_de_idade = idade >=18
+
+print('Nome:',nome,'idade:',idade)
+if maior_de_idade:
+    print('Verdadeiro')
---
+++ aula8.py
@@ -0,0 +1,14 @@
+#Exercicio Variaveis e tipos de dados
+nome = 'Elon Reeve'
+sobrenome = 'Musk'
+idade = 53
+ano_nascimento= 1971
+maior_de_idade = 'Sim' if idade>=18 else 'Não'
+altura_metros = 1.88
+
+print('Nome:',nome)
+print('Sobrenome:',sobrenome)
+print('Idade:',idade)
+print('Ano de nascimento:',ano_nascimento)
+print('É maior de idade?:',maior_de_idade)
+print('Altura em metros:',altura_metros)
