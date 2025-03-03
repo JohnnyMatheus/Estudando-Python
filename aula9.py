506ba3338080139a6a95b714c2a716131ebdfd0b
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-03 05:48:41 -0300

    Create aula9.py

---
+++ aula9.py
@@ -0,0 +1,29 @@
+#OPERADORES ARITMÉTICOS
+
+#ADIÇÃO
+adicao = 2+2
+print('Adição:',adicao)
+
+#SUBTRAÇÃO
+subtracao = 2-2
+print('Subtração:',subtracao)
+
+#MULTIPLICAÇÃO
+multiplicacao = 20*20
+print('Multiplicação:',multiplicacao)
+
+#DIVISÃO
+divisao = 20/5
+print('Divisão:',divisao)
+
+#DIVISÃO INTEIRA
+divisao_inteira = 20//5
+print('Divisão Inteira:',divisao_inteira)
+
+#EXPONENCIAÇÃO
+exponenciacao = 20**2
+print('Exponenciação:',exponenciacao)
+
+#MÓDULO - RESTO DA DIVISÃO
+modulo = 20%5
+print('Módulo:',modulo)
