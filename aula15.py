12e3b9b088f1aa3a41aa027cac2a515814a84b62
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-04 00:07:34 -0300

    Create aula15.py

---
+++ aula15.py
@@ -0,0 +1,20 @@
+#Entrada de dados
+
+nome = input('qual seu nome: ')
+print(f'O seu nome é: {nome =}')
+
+
+numero_1 = int(input('Digite um numero: '))
+numero_2 = int(input('Digite outro número: '))
+
+print(f'A soma dos números é: {numero_1 + numero_2}')
+
+print('-'*40)
+
+numero_1 = input('Digite um numero: ')
+numero_2 = input('Digite outro número: ')
+
+num1 = int(numero_1)
+num2 = int(numero_2)
+
+print(f'A soma dos números é: {numero_1 + numero_2}')
