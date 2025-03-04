877c24550994f8b774581b3644955bb3db795f6e
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-04 00:31:56 -0300

    Create aula17.py

---
+++ aula17.py
@@ -0,0 +1,27 @@
+#if /elif /else entendedo o fluxo
+
+condicao1 = True
+
+if condicao1:
+    print('Verdadeiro')
+else:
+    print('')    
+
+print('fora do if')
+
+print('#'*50)
+
+condicao = False
+
+if condicao:
+    print('Verdadeiro')
+else:
+    print('FALSO')    
+
+print('fora do if')
+
+print('#'*50)
+
+if  10 == 10:
+    print('V')
+else: print('F')    
