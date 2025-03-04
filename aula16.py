f49a0006a11815afe3edfd98d036275b04941159
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-04 00:19:38 -0300

    Create aula16.py

---
+++ aula16.py
@@ -0,0 +1,11 @@
+#Estruturas condicionais if / elif / else
+#se / senão se /se senao 
+
+entrada = input('Você deseja "entrar" ou "sair"?: ')
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+
+elif entrada == "sair": 
+    print('Você saiu do sistema')    
+else:
+    print('valor invalido')    
