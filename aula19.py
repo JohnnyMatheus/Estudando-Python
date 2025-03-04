d0c1bf187966e56e9b8c2e9f339747aa5d79e5de
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-04 16:15:13 -0300

    estudando

---
+++ aula19.py
@@ -0,0 +1,21 @@
+"""
+Oeradores  de comparação
+> maior
+< menor
+>= maior ou igual
+<= menor ou igual
+== igual
+!= diferente
+"""
+
+maior = 2 >1
+menor = 2<1
+maior_igual = 2 >=2
+menor_igual = 2 <=2
+igual = 2 ==2
+diferente = 10!= 1
+
+print(maior)
+print(menor)
+print(maior_igual)
+print(igual)
