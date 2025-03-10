97e8c9894ff900465916d9d86460a539bfb9952e
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-09 21:17:21 -0300

    Estudando

---
+++ aula27.py
@@ -0,0 +1,15 @@
+'''
+Fatiamento de Strings
+
+fatiamento[i:f:p] [::]
+inicio e fim
+
+'''
+variavel = 'Johnny Matheus'
+print(len(variavel))
+print(variavel[4:8])
+print(variavel[4:])
+print(variavel[:4])
+print(variavel[0:len(variavel):1]) #passo de 1 em 1
+print(variavel[0:len(variavel):2]) #passo de 2 em 2
+print(variavel[-1:-15:-1])
